0c7fac513ef296a37f840fcd7912e3e44f030756
Luis <luiz7@umes.edu.gt>
2025-03-28 00:34:42 -0600

    Creacion del microservicio 'identity' para la generacion y validacion del token

--- microservice-identity/src/main/java/gt/edu/umes/broker/broker/config/AuthConfig.java
+++ microservice-identity/src/main/java/gt/edu/umes/broker/broker/config/AuthConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-/*Hola*/
+
 @Configuration
 @EnableWebSecurity
 public class AuthConfig {

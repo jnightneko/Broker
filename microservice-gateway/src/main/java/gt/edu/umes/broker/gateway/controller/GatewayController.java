3aa34ff1165cd90fe0c9029d554adf924f0cba11
wil <wil@debian>
2025-04-13 20:02:50 -0600

    update

--- microservice-gateway/src/main/java/gt/edu/umes/broker/gateway/controller/GatewayController.java
+++ microservice-gateway/src/main/java/gt/edu/umes/broker/gateway/controller/GatewayController.java
@@ -9,32 +9,70 @@ import gt.edu.umes.broker.gateway.service.GatewayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Clase encargada el controlador de las peticiones, dichas peticiones provienen de los 
+ * cliente que desean conectarse con los servicios de la organización.
+ * 
+ * @author wil
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @RestController
 @RequestMapping("/broker/api/rest")
 public final class GatewayController {
     @Autowired
     private GatewayService service;
 
+    /**
+     * Petición por el método {@code GET} con el formtado estandar.
+     * 
+     * @param object objeto web
+     * @return respueta
+     */
     @GetMapping
     public Object requestGET(@RequestBody BKRequestModel object) {
         return service.send(object);
     }
 
+    /**
+     * Petición por el método {@code POST} con el formtado estandar.
+     * 
+     * @param object objeto web
+     * @return respueta
+     */
     @PostMapping
     public Object requestPOST(@RequestBody BKRequestModel object) {
         return service.send(object);
     }
 
+    /**
+     * Petición por el método {@code PUT} con el formtado estandar.
+     * 
+     * @param object objeto web
+     * @return respueta
+     */
     @PutMapping
     public Object requestPUT(@RequestBody BKRequestModel object) {
         return service.send(object);
     }
 
+    /**
+     * Petición por el método {@code PATH} con el formtado estandar.
+     * 
+     * @param object objeto web
+     * @return respueta
+     */
     @PatchMapping
-    public void requestPATCH(@RequestBody BKRequestModel object) {
-        service.send(object);
+    public Object requestPATCH(@RequestBody BKRequestModel object) {
+        return service.send(object);
     }
 
+    /**
+     * Petición por el método {@code DELETE} con el formtado estandar.
+     * 
+     * @param object objeto web
+     * @return respueta
+     */
     @DeleteMapping
     public Object requestDELETE(@RequestBody BKRequestModel object) {
         return service.send(object);

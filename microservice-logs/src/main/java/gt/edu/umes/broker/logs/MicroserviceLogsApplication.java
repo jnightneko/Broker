58d67f733629c9d2d02d5918925144ec5b4caf47
DavidFishy-18 <davidfishy@umes.edu.gt>
2025-04-15 17:35:09 -0600

    Modulo logs
    creacion del modulo de los logs

---
+++ microservice-logs/src/main/java/gt/edu/umes/broker/logs/MicroserviceLogsApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umes.broker.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceLogsApplication.class, args);
+	}
+
+}
---
+++ microservice-logs/src/test/java/gt/edu/umes/broker/logs/MicroserviceLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umes.broker.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceLogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

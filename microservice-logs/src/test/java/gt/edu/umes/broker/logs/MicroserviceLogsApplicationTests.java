3fe4dd9f0302860a9cd25ce465ec4c0d3ad76245
wil <wil@debian>
2025-05-02 23:04:05 -0600

    clear

--- microservice-logs/src/test/java/gt/edu/umes/broker/logs/MicroserviceLogsApplicationTests.java
+++ microservice-logs/src/test/java/gt/edu/umes/broker/logs/MicroserviceLogsApplicationTests.java
@@ -1,13 +1,8 @@
 package gt.edu.umes.broker.logs;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class MicroserviceLogsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    void contextLoads() {
+    }
 
 }

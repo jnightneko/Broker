b42b1a002e0dc121ee49963b2b876c2b30c0dcdf
luisantoniob18 <117749222+luisantoniob18@users.noreply.github.com>
2025-05-11 14:34:01 -0600

    cerrarSesion ya funciona (#39)
    * Implementacion inc blackList
    
    * Duda
    
    No reconoce 'JWTAuthorizationFilter'
    
    * Implementacion cerrarSesion
    
    Cerrar sesion ya funciona como se espeera, pendiente: encriptacion y aplicar correcciones
    
    * /POST/salir
    
    eliminacion del filtro
    
    * tokenHash
    
    Solo se guarda el hash del token en la bd
    
    * Update authService, identity
    
    Uso de hash en cerrarSesion()
    
    * Eliminacion buscarTokenPorValor()
    
    Metodo innecesario

--- core/src/main/java/gt/edu/umes/broker/core/system/SFPBSystem.java
+++ core/src/main/java/gt/edu/umes/broker/core/system/SFPBSystem.java
@@ -17,6 +17,8 @@ import javax.crypto.SecretKey;
  * @author wil
  */
 public final class SFPBSystem {
+    public static final String SECRET_KEY_ENCRYPT =  "a5d6e9f4b5c7d8a9b0c3d2e1f4a5b6c7";
+
     public static final String SECRET_KEY_TOKEN     = "e7f5a4c6d3b8e2f9a1c4d7e8f2b3c4a5d6e9f4b5c7d8a9b0c3d2e1f4a5b6c7";
     public static final Long TIME_EXPIRATION_TOKEN  = 1000L * 60L * 30L;
     

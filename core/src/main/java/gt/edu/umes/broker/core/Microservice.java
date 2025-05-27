5fb04ab2aa0bc7266bbe0be675430580db1ada4f
Neko <105011693+jnightneko@users.noreply.github.com>
2025-05-26 23:33:41 -0600

    Mapeo (#44)
    * map
    
    * update
    
    ---------
    
    Co-authored-by: wil <wil@debian>

--- core/src/main/java/gt/edu/umes/broker/core/Microservice.java
+++ core/src/main/java/gt/edu/umes/broker/core/Microservice.java
@@ -19,25 +19,25 @@ public final class Microservice {
     //** SERVICIOS EXTERBOS UTIIZADAS POR LA ORGANIZACIÓN
     //
     /** Direcciones pública del servicios: administración. */
-    public static final String MICROSERVICE_ADMIN       = "http://localhost:3000/";
+    public static final String MICROSERVICE_ADMIN       = "http://64.23.169.22:3000/";
     /** Direcciones pública del servicios: pagos. */
-    public static final String MICROSERVICE_PAGOS       = "http://localhost:3001/";
+    public static final String MICROSERVICE_PAGOS       = "http://64.23.169.22:3001/";
     /** Direcciones pública del servicios: gasolinera. */
-    public static final String MICROSERVICE_GAS         = "http://localhost:3002/";
+    public static final String MICROSERVICE_GAS         = "http://64.23.169.22:3002/";
     /** Direcciones pública del servicios: tineda de conveniencia. */
-    public static final String MICROSERVICE_TIENDA      = "http://localhost:3003/";
+    public static final String MICROSERVICE_TIENDA      = "http://64.23.169.22:3003/";
     /** Direcciones pública del servicios: teller y repuestos. */
-    public static final String MICROSERVICE_REPUESTOS   = "http://localhost:4000/";
+    public static final String MICROSERVICE_REPUESTOS   = "http://64.23.169.22:4000/";
     /** Direcciones pública del servicios: taller y pintutra. */
-    public static final String MICROSERVICE_PINTURA     = "http://localhost:8000/";
+    public static final String MICROSERVICE_PINTURA     = "http://64.23.169.22:8000/";
     
     //**************************************************************************************************************
     //** SERVICIOS INTERNOS DEL BROKER
     //    
     /** Microservicio conector. */
-    public static final String INTERNAL_MICROSERVICE_CONNECTOR = "http://localhost:8090";
+    public static final String INTERNAL_MICROSERVICE_CONNECTOR = "http://${HOST_NAME_CONNECTOR}:8090";
     /** Microservicio de logs. */
-    public static final String INTERNAL_MICROSERVICE_LOGS      = "http://localhost:8095";
+    public static final String INTERNAL_MICROSERVICE_LOGS      = "http://${HOST_NAME_LOGS}:8095";
     
     /**
      * Determina si una ip es válida o bien es conocido y es de confinza para permitirle
@@ -65,9 +65,9 @@ public final class Microservice {
     public static boolean bkAllowAddress(String address) {
         Server[] servers = Server.values();
         for (Server host : servers) {
-            if (host == Server.Null) {
-                continue;
-            }
+            //if (host == Server.Null) {
+            //    continue;
+            //}
             if (host.getAddress().equals(address)) {
                 return true;
             }

4495b614c87f2470c8b9708797b01de328a860c3
Neko <105011693+jnightneko@users.noreply.github.com>
2025-05-30 23:32:24 -0600

    Cors (#46)
    * cors
    
    * update
    
    ---------
    
    Co-authored-by: wil <wil@debian>

--- microservice-validation/src/main/java/gt/edu/umes/broker/validation/MicroserviceValidationApplication.java
+++ microservice-validation/src/main/java/gt/edu/umes/broker/validation/MicroserviceValidationApplication.java
@@ -14,6 +14,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 import static java.lang.System.setProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 /**
  * Clase principal del microservicio {@code microservice-validation}.
@@ -44,5 +55,17 @@ public class MicroserviceValidationApplication {
         /* SETTERS */
         setProperty("msvc.connector.host", "http://" + env.getProperty("HOST_NAME_CONNECTOR") + ":8090");
         setProperty("broker.logs.debug",env.getProperty("broker.logs.debug"));
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }

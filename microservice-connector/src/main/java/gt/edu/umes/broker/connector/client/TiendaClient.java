7858a02b86b642821e9d795ba1cbda4fa9a36cca
wil <wil@debian>
2025-05-30 23:11:33 -0600

    map|tienda

--- microservice-connector/src/main/java/gt/edu/umes/broker/connector/client/TiendaClient.java
+++ microservice-connector/src/main/java/gt/edu/umes/broker/connector/client/TiendaClient.java
@@ -15,45 +15,105 @@ import org.springframework.web.bind.annotation.*;
  * enlaces a los endpoints de dicho servicio.
  * 
  * @author wil
- * @version 1.0.0
+ * @version 2.0.0
  * @since 1.0.0
  */
 @FeignClient(value = "MCSV-TiendaConveniencia", url = Microservice.MICROSERVICE_TIENDA)
 public interface TiendaClient {
-    /*(non-Javadoc)*/
+    
+    @GetMapping(HTTP_PRODUCTOS_GET)
+    Object func1();
+    
+    @PostMapping(HTTP_PRODUCTOS_POST)
+    Object f2(@RequestBody Object o);
+    
+    @GetMapping(HTTP_PRODUCTOS_GET_ID)
+    Object f3(@PathVariable("id") Object o);
+    
+    @PutMapping(HTTP_PRODUCTOS_PUT)
+    Object f4(@PathVariable("id") Object o);
+    
+    @DeleteMapping(HTTP_PRODUCTO_DELETE)
+    Object f5(@PathVariable("id") Object o);
+    
+    @GetMapping(HTTP_CATEGORIA_GET)
+    Object f6();
+    
+    @PostMapping(HTTP_CATEGORIA_POST)
+    Object f7(@RequestBody Object o);
+    
+    
+    
+    
+    @GetMapping(HTTP_CATEGORIA_GET_ID)
+    Object f8(@PathVariable("id") Object o);
+    
+    @PutMapping(HTTP_CATEGORIA_PUT)
+    Object f9(@PathVariable("id") Object id,@RequestBody Object o);
+    
+    @DeleteMapping(HTTP_CATEGORIA_DELETE)
+    Object f10(@PathVariable("id") Object id);
+    
+    @GetMapping(HTTP_MARCAS_GET)
+    Object f11();
+    
+    @PostMapping(HTTP_MARCAS_POST)
+    Object f12(@RequestBody Object o);
+    
+    @GetMapping(HTTP_MARCAS_GET_ID)
+    Object f13(@PathVariable("id") Object o);
+    
+    @PutMapping(HTTP_MARCAS_PUT)
+    Object f14(@PathVariable("id") Object o1, @RequestBody Object o);
+    
+    @DeleteMapping(HTTP_MARCAS_DELETE)
+    Object f15(@PathVariable("id") Object i);
+    
+    
+    
+    
+    @PostMapping(HTTP_LOTES_POST)
+    Object f16(@RequestBody Object o);
+    
+    @GetMapping(HTTP_LOTES_GET)
+    Object f17();
+    
+    @GetMapping(HTTP_LOTES_GET_PRODUCTO_ID)
+    Object f18(@PathVariable("idProducto") Object id);
+    
+    @GetMapping(HTTP_LOTES_GET_ID)
+    Object f19(@PathVariable("id") Object id);
+    
+    @PutMapping(HTTP_LOTES_PUT)
+    Object f20(@PathVariable("id") Object o, @RequestBody Object h);
+    
+    @DeleteMapping(HTTP_LOTES_DELETE)
+    Object f21(@PathVariable("id") Object id);
+    
     @GetMapping(HTTP_VENTAS_GET)
-    public Object nVentaGet();
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_VENTAS_COMPLETO_GET)
-    public Object nVentasGetCompleto();
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_VENTAS_ID_GET)
-    public Object nVentasGetId(@PathVariable("id") Object id);
-    /*(non-Javadoc)*/
+    Object f22();
+    
     @PostMapping(HTTP_VENTAS_POST)
-    public Object nVentasPost(@RequestBody Object o);
-    /*(non-Javadoc)*/
+    Object f23(@RequestBody Object s);
+    
+    @GetMapping(HTTP_VENTAS_ID_GET)
+    Object f24(@PathVariable("id") Object id);
+    
     @PutMapping(HTTP_VENTAS_PUT)
-    public Object nVentasPut(@PathVariable("id") Object id, @RequestBody Object o);
-    /*(non-Javadoc)*/
-    @DeleteMapping(HTTP_VENTAS_DELETE)
-    public Object nVentaDelete(@PathVariable("id") Object id);
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_DEVOLUCIONES_GET)
-    public Object nDevolucionesGet();
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_DEVOLUCIONES_ID_GET)
-    public Object nDevolucionesGetId(@PathVariable("id") Object id);
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_DEVOLUCIONES_VENTA_GET)
-    public Object nDevolucionesVentaGet(@PathVariable("id") Object id);
-    /*(non-Javadoc)*/
-    @GetMapping(HTTP_DEVOLUCIONES_PENDIENTE_GET)
-    public Object nDevolucionesPendienteGet();
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_POST)
-    public Object nDevolucionesPost(@RequestBody Object o);
-    /*(non-Javadoc)*/
-    @PutMapping(HTTP_DEVOLUCIONES_PUT)
-    public Object nDevolcionesPut(@PathVariable("id") Object id, @RequestBody Object o);
+    Object f25(@PathVariable("id") Object s, @RequestBody Object o);
+    
+    @GetMapping(HTTP_RECETAS_GET)
+    Object f26(@PathVariable("idProducto") Object id);
+    
+    @PutMapping(HTTP_RECETAS_PUT)
+    Object f27(@PathVariable("idProducto") Object id, @RequestBody Object o);
+    
+    @PostMapping(HTTP_RECETAS_POST)
+    Object f28(@RequestBody Object o);
+    
+    @DeleteMapping(HTTP_RECETAS_DELETE)
+    Object f29(@PathVariable("idAsignacion") Object o);
+    
+    @PatchMapping(HTTP_RECETA_PATCH)
+    Object f30(@PathVariable("idAsignacion") Object i, @RequestBody Object oi);
 }
--- microservice-connector/src/main/java/gt/edu/umes/broker/connector/controller/TiendaController.java
+++ microservice-connector/src/main/java/gt/edu/umes/broker/connector/controller/TiendaController.java
@@ -13,7 +13,7 @@ import static gt.edu.umes.broker.core.endpoints.MCSVTienda.*;
 /**
  * Clase encargado de gestionar el redireccionamiento de las peticiones a las direcciones correctas.
  * @author wil
- * @version 1.0.0
+ * @version 1.2.0
  * @since 1.0.0
  */
 @RestController
@@ -21,64 +21,155 @@ import static gt.edu.umes.broker.core.endpoints.MCSVTienda.*;
 public class TiendaController {
     @Autowired
     private TiendaClient client;
-    /*(non-Javadoc)*/
+    
+    
+    @PostMapping(HTTP_PRODUCTOS_GET)
+    public Object func1() {
+        return client.func1();
+    }
+    
+    @PostMapping(HTTP_PRODUCTOS_POST)
+    public Object f2(@RequestBody Object o) {
+        return client.f2(o);
+    }
+    
+    @PostMapping(HTTP_PRODUCTOS_GET_ID)
+    public Object f3(@PathVariable("id") Object o) {
+        return client.f3(o);
+    }
+    
+    @PostMapping(HTTP_PRODUCTOS_PUT)
+    public Object f4(@PathVariable("id") Object o) {
+        return client.f4(o);
+    }
+    
+    @PostMapping(HTTP_PRODUCTO_DELETE)
+    public Object f5(@PathVariable("id") Object o) {
+        return client.f5(o);
+    }
+    
+    @PostMapping(HTTP_CATEGORIA_GET)
+    public Object f6() {
+        return client.f6();
+    }
+    
+    @PostMapping(HTTP_CATEGORIA_POST)
+    public Object f7(@RequestBody Object o) {
+        return client.f7(o);
+    }
+    
+    @PostMapping(HTTP_CATEGORIA_GET_ID)
+    public Object f8(@PathVariable("id") Object o) {
+        return client.f8(o);
+    }
+    
+    @PostMapping(HTTP_CATEGORIA_PUT)
+    public Object f9(@PathVariable("id") Object id,@RequestBody Object o) {
+        return client.f9(id, o);
+    }
+    
+    @PostMapping(HTTP_CATEGORIA_DELETE)
+    public Object f10(@PathVariable("id") Object id) {
+        return client.f10(id);
+    }
+    
+    @PostMapping(HTTP_MARCAS_GET)
+    public Object f11() {
+        return client.f11();
+    }
+    
+    @PostMapping(HTTP_MARCAS_POST)
+    public Object f12(@RequestBody Object o) {
+        return client.f12(o);
+    }
+    
+    @PostMapping(HTTP_MARCAS_GET_ID)
+    public Object f13(@PathVariable("id") Object o) {
+        return client.f13(o);
+    }
+    
+    @PostMapping(HTTP_MARCAS_PUT)
+    public Object f14(@PathVariable("id") Object o1, @RequestBody Object o) {
+        return client.f14(o1, o);
+    }
+    
+    @PostMapping(HTTP_MARCAS_DELETE)
+    public Object f15(@PathVariable("id") Object i) {
+        return client.f15(i);
+    }
+    
+    @PostMapping(HTTP_LOTES_POST)
+    public Object f16(@RequestBody Object o) {
+        return client.f16(o);
+    }
+    
+    @PostMapping(HTTP_LOTES_GET)
+    public Object f17() {
+        return client.f17();
+    }
+    
+    @PostMapping(HTTP_LOTES_GET_PRODUCTO_ID)
+    public Object f18(@PathVariable("idProducto") Object id) {
+        return client.f18(id);
+    }
+    
+    @PostMapping(HTTP_LOTES_GET_ID)
+    public Object f19(@PathVariable("id") Object id) {
+        return client.f19(id);
+    }
+    
+    @PostMapping(HTTP_LOTES_PUT)
+    public Object f20(@PathVariable("id") Object o, @RequestBody Object h) {
+        return client.f20(o, h);
+    }
+    
+    @PostMapping(HTTP_LOTES_DELETE)
+    public Object f21(@PathVariable("id") Object id) {
+        return client.f21(id);
+    }
+    
     @PostMapping(HTTP_VENTAS_GET)
-    public Object getVentaGet() {
-        return client.nVentaGet();
+    public Object f22() {
+        return client.f22();
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_VENTAS_COMPLETO_GET)
-    public Object getVentasGetCompleto() {
-        return client.nVentasGetCompleto();
-    }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_VENTAS_ID_GET)
-    public Object getVentasGetId(@PathVariable("id") Object id) {
-        return client.nVentasGetId(id);
-    }
-    /*(non-Javadoc)*/
+    
     @PostMapping(HTTP_VENTAS_POST)
-    public Object getVentasPost(@RequestBody Object o) {
-        return client.nVentasPost(o);
+    public Object f23(@RequestBody Object s) {
+        return client.f23(s);
     }
-    /*(non-Javadoc)*/
+    
+    @PostMapping(HTTP_VENTAS_ID_GET)
+    public Object f24(@PathVariable("id") Object id) {
+        return client.f24(id);
+    }
+    
     @PostMapping(HTTP_VENTAS_PUT)
-    public Object getVentasPut(@PathVariable("id") Object id, @RequestBody Object o) {
-        return client.nVentasPut(id, o);
+    public Object f25(@PathVariable("id") Object s, @RequestBody Object o) {
+        return client.f25(s, o);
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_VENTAS_DELETE)
-    public Object getVentaDelete(@PathVariable("id") Object id) {
-        return client.nVentaDelete(id);
+    
+    @PostMapping(HTTP_RECETAS_GET)
+    public Object f26(@PathVariable("idProducto") Object id) {
+        return client.f26(id);
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_GET)
-    public Object getDevolucionesGet() {
-        return client.nDevolucionesGet();
+    
+    @PostMapping(HTTP_RECETAS_PUT)
+    public Object f27(@PathVariable("idProducto") Object id, @RequestBody Object o) {
+        return client.f27(id, o);
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_ID_GET)
-    public Object getDevolucionesGetId(@PathVariable("id") Object id) {
-        return client.nDevolucionesGetId(id);
+    
+    @PostMapping(HTTP_RECETAS_POST)
+    public Object f28(@RequestBody Object o) {
+        return client.f28(o);
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_VENTA_GET)
-    public Object getDevolucionesVentaGet(@PathVariable("id") Object id) {
-        return client.nDevolucionesVentaGet(id);
+    
+    @PostMapping(HTTP_RECETAS_DELETE)
+    public Object f29(@PathVariable("idAsignacion") Object o) {
+        return client.f29(o);
     }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_PENDIENTE_GET)
-    public   Object getDevolucionesPendienteGet() {
-        return client.nDevolucionesPendienteGet();
-    }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_POST)
-    public Object getDevolucionesPost(@RequestBody Object o) {
-        return client.nDevolucionesPost(o);
-    }
-    /*(non-Javadoc)*/
-    @PostMapping(HTTP_DEVOLUCIONES_PUT)
-    public Object getDevolcionesPut(@PathVariable("id") Object id, @RequestBody Object o) {
-        return client.nDevolcionesPut(id, o);
+    
+    @PostMapping(HTTP_RECETA_PATCH)
+    public Object f30(@PathVariable("idAsignacion") Object i, @RequestBody Object oi) {
+        return client.f30(i, oi);
     }
 }

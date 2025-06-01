f0a93132aa79e12a73f7af349d30b9363c60697c
Neko <105011693+jnightneko@users.noreply.github.com>
2025-05-31 20:27:55 -0600

    update|cors (#49)
    Co-authored-by: wil <wil@debian>

--- microservice-identity/src/main/java/gt/edu/umes/broker/identity/IdentityServiceApplication.java
+++ microservice-identity/src/main/java/gt/edu/umes/broker/identity/IdentityServiceApplication.java
@@ -4,15 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -21,17 +12,5 @@ public class IdentityServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IdentityServiceApplication.class, args);
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 }

9601ba8e521b6ce09828b25b752aa93070434886
wil <wil@debian>
2025-05-31 20:20:38 -0600

    update|cors

--- microservice-connector/src/main/java/gt/edu/umes/broker/connector/MicroserviceConnectorApplication.java
+++ microservice-connector/src/main/java/gt/edu/umes/broker/connector/MicroserviceConnectorApplication.java
@@ -8,15 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 
 /**
  * Clase principal del microservicios <strong>connector</strong> encargado del
@@ -36,19 +27,5 @@ public class MicroserviceConnectorApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceConnectorApplication.class, args);
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Collections.singletonList("*"));
-        config.setAllowedOriginPatterns(Collections.singletonList("*"));
-        config.setAllowPrivateNetwork(true);
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 }
